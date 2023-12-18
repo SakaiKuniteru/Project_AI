bdc9eddf3d10b05fc9b3178fe828b392cb708cef
Phạm Quốc Huy <quochuyks2k3@gmail.com>
2023-12-18 15:31:58 +0700

    Update cost matrix

---
+++ cost_matrix.py
@@ -0,0 +1,30 @@
+import random
+import sys
+
+def generate_cost_matrix(num_cities, seed):
+    random.seed(seed)
+    cost_matrix = [[0] * num_cities for _ in range(num_cities)]
+
+    for i in range(num_cities):
+        for j in range(i + 1, num_cities):
+            cost = random.randint(1, 100)
+            cost_matrix[i][j] = cost
+            cost_matrix[j][i] = cost
+
+    return cost_matrix
+
+# Example usage:
+seeds = [1, 2, 3, 4, 5]
+num_cities_values = [5, 10, 11, 12]
+
+for num_cities in num_cities_values:
+    for seed in seeds:
+        cost_matrix = generate_cost_matrix(num_cities, seed)
+        
+        # Set console encoding to UTF-8
+        sys.stdout.reconfigure(encoding='utf-8')
+
+        print(f"n = {num_cities}, Seed = {seed}")
+        for row in cost_matrix:
+            print(row)
+        print()
